460389d7aa667f1b1e8de1a2d99cce5174dd8b31
Jorge Rabello <jorge.rabello3@gmail.com>
2018-11-17 18:52:03 -0200

    Activity modificada para utilizar a activity de base

--- app/src/main/java/me/indiq/dev/indiqme/LoginActivity.kt
+++ app/src/main/java/me/indiq/dev/indiqme/LoginActivity.kt
@@ -2,10 +2,9 @@ package me.indiq.dev.indiqme
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
