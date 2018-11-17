e94bbe37223889a46e6d544d554bfa563b8d44ec
Jorge Rabello <jorge.rabello3@gmail.com>
2018-11-17 18:37:54 -0200

    Criada navegação para a tela principal do app

--- app/src/main/java/me/indiq/dev/indiqme/LoginActivity.kt
+++ app/src/main/java/me/indiq/dev/indiqme/LoginActivity.kt
@@ -1,12 +1,19 @@
 package me.indiq.dev.indiqme
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_entrar.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
